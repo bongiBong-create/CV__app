e6531efd42ad88a443e74852bc7d1ae48f77918f
bongiBong-create <mairanss543@inbox.ru>
2024-10-13 21:11:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Personal from "./components/Personal";
+import "./styles/app.css";
+
+function App() {
+  return (
+    <main className="main">
+    <Personal />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Personal.jsx
@@ -0,0 +1,20 @@
+export default function Personal() {
+  return (
+    <section className="personal">
+      <form>
+        <div className="form__item">
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" placeholder="your name"/>
+        </div>
+        <div className="form__item">
+          <label htmlFor="last-name">Name</label>
+          <input id="last-name" type="text" placeholder="your last name"/>
+        </div>
+        <div className="form__item">
+          <label htmlFor="email">Email</label>
+          <input type="email" placeholder="example@gmail.com"/>
+        </div>
+      </form>
+    </section>
+  )
+}
